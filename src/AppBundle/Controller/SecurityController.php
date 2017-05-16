f348a778fe4be051e17fc7a535058fba7b52d2a7
Andrey <ivanov@ukrtatnafta.com>
2017-05-16 16:06:08 +0300

    second commit

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -25,12 +25,24 @@ class SecurityController extends Controller {
                     'error' => $error,
         ));
     }
-    
+
     /**
      * @Route("/login-image", name="login-image")
      */
-
     public function loginImageAction() {
-        return \PHPQRCode\QRcode::png('PHP QR Code :)');
+        // we need to be sure ours script does not output anything!!!
+        // otherwise it will break up PNG binary!
+
+        ob_start();
+
+        // here DB request or some processing
+        $codeText = 'DEMO - ' . '*******   // we need to be sure ours script does not output anything!!!
+        // otherwise it will break up PNG binary!';
+
+        // end of processing here
+        $debugLog = ob_get_contents();
+        ob_end_clean();
+        return \PHPQRCode\QRcode::png($codeText);
     }
+
 }
